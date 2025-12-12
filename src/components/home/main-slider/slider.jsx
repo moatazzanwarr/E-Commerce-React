f78447bf0076484760da9cdc7807ffb27032841f
Moataz Anwar <moatazzanwarr@gmail.com>
2025-12-12 15:45:47 +0200

    after active main slider

--- src/components/home/main-slider/slider.jsx
+++ src/components/home/main-slider/slider.jsx
@@ -1,29 +1,58 @@
 // Imports
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./slider.css";
 
 // Icons
 import icons from "../../../assets/icons";
 
+const slides = [
+  {
+    id: 1,
+    title: `Fresh Vegetables Big discount`,
+    desc: "Save up to 50% off on your first order 1111",
+  },
+  {
+    id: 2,
+    title: `Fresh Vegetables Big discount`,
+    desc: "Save up to 50% off on your first order 2222",
+  },
+];
 
 function Slider() {
+  const [slideNumber, setSlideNumber] = useState(1);
+
+      const next = ()=>{
+        return setSlideNumber(prev => prev === 1 ? prev + 1 : prev - 1 )
+      }
+
   return (
-    <div className="main-slider">
-      <div className="slide-1 slide">
-        <div className="text">
-          <h1>Fresh Vegetables <br/>Big discount</h1>
-          <span>Save up to 50% off on your first order</span>
-          <form action="">
-            <input type="text" placeholder="Your emaill address" />
-            <input type="submit" value="Subscribe" />
-          </form>
-        </div>
+    <div className="main">
+      <div className="container-slides">
+        {slides.map((e, i) => {
+          return (
+            <div
+              key={i}
+              className={`slide ${
+                e.id === slideNumber ? "active" : "notActive"
+              }`}
+            >
+              <div className="text">
+                <h1>{e.title}</h1>
+                <span>{e.desc}</span>
+                <form action="">
+                  <input type="text" placeholder="Your Email Address" />
+                  <input type="submit" value="Subscribe" />
+                </form>
+              </div>
+            </div>
+          );
+        })}
       </div>
 
-      <span>
+      <span onClick={()=> next()}>
         <icons.ArrowCircleLeftIcon className="slide-left i" />
       </span>
-      <span>
+      <span onClick={()=> next()}>
         <icons.ArrowCircleRightIcon className="slide-right i" />
       </span>
     </div>
